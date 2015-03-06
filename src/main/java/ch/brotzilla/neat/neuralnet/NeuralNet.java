69c84a6e2a30d7cd74954fa98b04d0380bafede1
Manuel Brotz <manu.brotz@gmx.ch>
2015-03-06 22:58:24 +0100

    Added TODO to class NeuralNet.

--- src/main/java/ch/brotzilla/neat/neuralnet/NeuralNet.java
+++ src/main/java/ch/brotzilla/neat/neuralnet/NeuralNet.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import com.google.common.base.Preconditions;
 
+// TODO Add NeuronType to class Neuron to make it possible to distinguish hidden/output neurons. For preconditions.
 public class NeuralNet {
 
     private final int numberOfInputNeurons, numberOfHiddenNeurons, numberOfOutputNeurons;
